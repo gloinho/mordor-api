74d511d06d966249d4a4450627145206847684e9
Anastacio <gl0inn@hotmail.com>
2023-08-22 17:35:18 -0300

    Add project files.

---
+++ Controllers/MordorController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace mordor_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MordorController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize(Roles = "CreateRing")]
+        public ActionResult CreateRing()
+        {
+            return Ok("Ash nazg durbatulûk, ash nazg gimbatul, ash nazg thrakatulûk, agh burzum-ishi krimpatul");
+        }
+
+        [HttpDelete]
+        [Authorize(Roles ="DestroyRing")]
+        public ActionResult DestroyRing()
+        {
+            return NoContent();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+using Keycloak.AuthServices.Authentication;
+using Keycloak.AuthServices.Authorization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var authenticationOptions = builder
+                            .Configuration
+                            .GetSection(KeycloakAuthenticationOptions.Section)
+                            .Get<KeycloakAuthenticationOptions>();
+
+builder.Services.AddKeycloakAuthentication(authenticationOptions);
+
+var authorizationOptions = builder
+                            .Configuration
+                            .GetSection(KeycloakProtectionClientOptions.Section)
+                            .Get<KeycloakProtectionClientOptions>();
+
+builder.Services.AddKeycloakAuthorization(authorizationOptions);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
